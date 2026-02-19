f200418d0bb82100d5eefd5699178815406c359c
Dipti Pariya <diptipariya@gmail.com>
2026-02-19 17:24:02 +0530

    sample app - simple server is running

---
+++ index.js
@@ -0,0 +1,5 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.end("Hello from sample app!");
+});
+server.listen(3005, () => console.log("Sample app running"));
