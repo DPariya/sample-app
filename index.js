e0446e9856df9f9bebf5fa3c6372c97e8b43636c
Dipti Pariya <diptipariya@gmail.com>
2026-02-27 15:26:44 +0530

    test

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.end("Hello from sample app!!!");
 });
-server.listen(3005, () => console.log("Sample app running!"));
+server.listen(3005, () => console.log("Sample app running on port 3005!"));
